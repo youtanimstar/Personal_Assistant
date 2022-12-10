cb5c525b50bcc8d2adb482be01bccaab8d7f504a
youtanimstar <animdeep2019@gmail.com>
2022-12-10 22:13:45 +0530

    new commit

--- js/bookmark-window.js
+++ js/bookmark-window.js
@@ -4,6 +4,7 @@ const bookmarkWindowBackground = document.querySelector(
 );
 const cross = document.querySelector(".fa-x");
 const bookmarkAdd = document.querySelectorAll(".bookmark-add");
+const bookmarkSave = document.querySelector(".bookmark-window-save");
 
 bookmarkAdd[0].addEventListener("click", () => {
   bookmarkWindow.classList.toggle("option-display");
@@ -21,3 +22,45 @@ bookmarkWindowBackground.addEventListener("click", () => {
   bookmarkWindow.classList.toggle("option-display");
   bookmarkWindowBackground.classList.toggle("option-display");
 });
+bookmarkSave.addEventListener("click", () => {
+  setTimeout(() => {
+    bookmarkWindow.classList.toggle("option-display");
+    bookmarkWindowBackground.classList.toggle("option-display");
+  }, 1000);
+});
+
+bookmarkSave.addEventListener("click", (text = "") => {
+  const bookmarkWindowName = document.querySelector(
+    ".bookmark-window-name"
+  ).value;
+  const bookmarkWindowURL = document.querySelector(
+    ".bookmark-window-url"
+  ).value;
+
+  const bookmarkOption = document.createElement("a");
+  bookmarkOption.classList.add("sub-option");
+  const BookmarkoptionhtmlData = `<a href="${bookmarkWindowURL}" class="sub-option-link" target="blank"> ${bookmarkWindowName}</a>`;
+
+  bookmarkOption.insertAdjacentHTML('afterbegin', BookmarkoptionhtmlData);
+  const subSection = document.querySelector(".sub-section").appendChild(bookmarkOption);
+  
+});
+bookmarkSave.addEventListener("click", (text = "") => {
+  const bookmarkWindowName = document.querySelector(
+    ".bookmark-window-name"
+  ).value;
+  const bookmarkWindowURL = document.querySelector(
+    ".bookmark-window-url"
+  ).value;
+
+  const bookmarkOption = document.createElement("a");
+  bookmarkOption.classList.add("sub-option");
+  const BookmarkoptionhtmlData = `<a href="${bookmarkWindowURL}" class="sub-option-link" target="blank"> ${bookmarkWindowName}</a>`;
+
+  bookmarkOption.insertAdjacentHTML('afterbegin', BookmarkoptionhtmlData);
+  const bookmarkSectionContainer = document.querySelector(".bookmark-section-container").appendChild(bookmarkOption);
+
+  
+});
+
+
